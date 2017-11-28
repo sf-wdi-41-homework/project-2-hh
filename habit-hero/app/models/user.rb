974cf2b933eb241ce9c622cf7f74854ea39799a6
Ray <xuejuhui@hotmail.com>
2017-11-27 16:43:34 -0800

    user auth done

--- habit-hero/app/models/user.rb
+++ habit-hero/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+	has_secure_password
 	has_many :habits
+		def self.confirm(params)
+    	@user = User.find_by({username: params[:username]})
+    	@user ? @user.authenticate(params[:password]) : false
+	end 
 end
