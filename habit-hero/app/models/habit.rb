4b3f68f293cba23860843397a54bba7dc77124f3
snopeks <creativesnopeks@gmail.com>
2017-11-28 14:36:41 -0800

    add dependent destroy for habit model

--- habit-hero/app/models/habit.rb
+++ habit-hero/app/models/habit.rb
@@ -1,4 +1,4 @@
 class Habit < ApplicationRecord
   belongs_to :user
-  has_many :logged_habits 
+  has_many :logged_habits, dependent: :destroy
 end
--- habit-hero/app/models/user.rb
+++ habit-hero/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 	has_secure_password
-	has_many :habits
+	has_many :habits, dependent: :destroy
 		def self.confirm(params)
     	@user = User.find_by({username: params[:username]})
     	@user ? @user.authenticate(params[:password]) : false
-	end 
+	end
 end
