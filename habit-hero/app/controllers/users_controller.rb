1f88bdefb0080f753748378aa8cc428afbf533ab
Ray <xuejuhui@hotmail.com>
2017-11-28 14:57:53 -0800

    add profile editing function

--- habit-hero/app/controllers/users_controller.rb
+++ habit-hero/app/controllers/users_controller.rb
@@ -21,6 +21,16 @@ class UsersController < ApplicationController
     @user = User.find_by_id(params[:id])
 		@habits = @user.habits 
   	end
+  	def edit
+  		@user = User.find_by_id(params[:id])
+  	end 
+  	def update
+  		@user = User.find_by_id(params[:id])
+  		@user.update(user_params)
+  		redirect_to @user
+  	end 
+
+
 	private
 	def user_params
 	params.require(:user).permit(:first_name, :last_name, :email, :password, :username)
