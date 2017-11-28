cefc0578062bb1ebd0def3b3e199f32945fa2add
Ray <xuejuhui@hotmail.com>
2017-11-28 12:16:33 -0800

    added crud for habits

---
+++ habit-hero/app/controllers/habits_controller.rb
@@ -0,0 +1,31 @@
+class HabitsController < ApplicationController
+	def index
+		user = current_user
+		@habits = user.habits
+	end
+	def new 
+		@habit = Habit.new
+	end 
+	def create
+		user = current_user
+		@habit = user.habits.new(habit_params)
+		@habit.save
+		if @habit.save
+			redirect_to "/habits"
+		else 
+			redirect_to "/habits"
+		end 
+	end 
+	def destroy
+		user = current_user
+		habits = user.habits.all
+		habit = habits.find(params[:id])
+		habit.destroy
+		redirect_to "/habits"
+	end
+
+	private 
+	def habit_params
+		params.require(:habit).permit(:title, :description,:weekly_goal)
+	end 
+end
