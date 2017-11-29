040bfc13578e31548a2ef1acddb4b0204fcd7ab8
snopeks <creativesnopeks@gmail.com>
2017-11-29 11:32:58 -0800

    add signup validation for password length and username uniqueness

--- habit-hero/app/models/user.rb
+++ habit-hero/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	validates :password, presence: true, length: {minimum: 8}
+	validates :username, presence: true, uniqueness: true
 	has_secure_password
 	has_many :habits, dependent: :destroy
 	has_many :logged_habits, dependent: :destroy
