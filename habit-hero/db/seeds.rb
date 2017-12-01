7f4ef4e56dc8f6dda6c6a850697082f4ff5af604
snopeks <creativesnopeks@gmail.com>
2017-12-01 12:33:02 -0800

    add seed data and fix weekly view day numerals for setting monday as start of week

--- habit-hero/db/seeds.rb
+++ habit-hero/db/seeds.rb
@@ -5,3 +5,142 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_seed_data = [
+  {
+    first_name: "Sherry",
+    last_name: "Cheng",
+    email: 's@c.com',
+    username: "scheng",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Nick",
+    last_name: "Lee",
+    email: 'n@l.com',
+    username: "nlee",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Ray",
+    last_name: "Xue",
+    email: 'r@x.com',
+    username: "rayman",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Beth",
+    last_name: "Witten",
+    email: 'b@w.com',
+    username: "bwitty",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Steph",
+    last_name: "Snopek",
+    email: 's@s.co',
+    username: "snopeks",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Peng",
+    last_name: "Ye",
+    email: 'p@y.com',
+    username: "pengisgreat",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Sherwin",
+    last_name: "Shaan",
+    email: 's@s.com',
+    username: "sherwinner",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Austin",
+    last_name: "Olsen",
+    email: 'a@o.com',
+    username: "aolol",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Levi",
+    last_name: "Perry",
+    email: 'l@p.com',
+    username: "lperry",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Kay",
+    last_name: "Nil",
+    email: 'k@n.com',
+    username: "heykay",
+    password: "happypuppy",
+  },
+  {
+    first_name: "Justin",
+    last_name: "Castilla",
+    email: 'j@c.com',
+    username: "jdawg",
+    password: "happypuppy",
+  },
+]
+
+habit_seed_data = [
+  {
+    title: "sleep 8 hours",
+    description: "sleep 8 hours every night",
+    weekly_goal: 7
+  },
+  {
+    title: "cook healthy meals",
+    description: "cook healthy meals and save money three times a week",
+    weekly_goal: 3
+  },
+  {
+    title: "exercise",
+    description: "get the blood pumping and muscles moving a few times a week!",
+    weekly_goal: 5
+  },
+  {
+    title: "code a new project",
+    description: "code a new project every week to keep skills fresh",
+    weekly_goal: 1
+  },
+  {
+    title: "practice react/redux",
+    description: "work on a react/redux project every week, for a couple days at least",
+    weekly_goal: 2
+  }
+]
+log_seed_data = [
+  DateTime.new(2017, 11, 27, 4, 5, 6),
+  DateTime.new(2017, 11, 28, 3, 2, 4),
+  DateTime.new(2017, 11, 29, 2, 5, 6),
+  DateTime.new(2017, 11, 30, 6, 7, 8),
+  DateTime.new(2017, 11, 21, 6, 7, 8),
+  DateTime.new(2017, 11, 21, 6, 7, 8),
+  DateTime.new(2017, 11, 21, 6, 7, 8),
+  DateTime.new(2017, 11, 21, 6, 7, 8),
+  DateTime.new(2017, 11, 15, 6, 7, 8),
+  DateTime.new(2017, 11, 15, 6, 7, 8),
+  DateTime.new(2017, 11, 15, 6, 7, 8)
+]
+
+User.destroy_all
+
+user_seed_data.each do |user|
+  User.create({first_name: user[:first_name], last_name: user[:last_name], email: user[:email], username: user[:username], password: user[:password] })
+end
+
+User.all.each do |user|
+  habit_seed_data.each do |habit|
+    Habit.create({title: habit[:title], description: habit[:description], weekly_goal: habit[:weekly_goal], user_id: user.id})
+  end
+end
+
+Habit.all.each do |habit|
+  log_seed_data.each do |log|
+    LoggedHabit.create({date_completed: log, habit_id: habit.id, user_id: habit.user.id})
+  end
+end
