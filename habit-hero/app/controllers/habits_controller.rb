679e6d336c43658f157f2b53b13d29d3f50cc615
Dev <dev@divethree.com>
2017-12-05 12:23:26 -0800

    Code review.

--- habit-hero/app/controllers/habits_controller.rb
+++ habit-hero/app/controllers/habits_controller.rb
@@ -1,17 +1,14 @@
 class HabitsController < ApplicationController
 	def index
-		user = current_user
-		habits = user.habits
-		@habits = []
-		habits.each_with_index do |habit, i|
-			logs = LoggedHabit.where({user_id: user.id,habit_id: habit.id, date_completed: Time.now.midnight..(Time.now.midnight + 1.day) })
-			logs.blank? ? @habits << {habit_obj:habit, completed: false} : @habits << {habit_obj:habit, completed: true}
-		end
+		# FIXME: Change #1 - Simplify. No need to reassign current_user to user
+		@habits = current_user.habits
+
+  	# FIXME: Change #2 - Use scopes. See models/logged_habit.rb and views/habits/index.html.erb
+		@logged_habits = current_user.logged_habits.completed
 	end
 
 	def create
-		user = current_user
-		@habit = user.habits.new(habit_params)
+		@habit = current_user.habits.new(habit_params)
 		if @habit.save
 			redirect_to daily_tracker_path
 		else
@@ -20,45 +17,32 @@ class HabitsController < ApplicationController
 	end
 
 	def edit
-		user = current_user
-		habits = user.habits.all
+		habits = Habit.all
 		@habit = habits.find(params[:id])
 	end
 
 	def update
-		user = current_user
-		habits = user.habits.all
+		habits = Habit.all
 		@habit = habits.find(params[:id])
 		@habit.update(habit_params)
 		redirect_to show_habit_path(@habit)
 	end
 
 	def show
-		user = current_user
-		habits = user.habits.all
-		@habit = habits.find(params[:id])
-		weekly_count = @habit.logged_habits.group_by_week(:date_completed, week_start: :mon).count
-		@weekly_count = {}
-		@max_percent = 100
-		weekly_count.each_pair do | key, value |
-			percent = (value.to_f / @habit.weekly_goal.to_f) * 100.00
-			@weekly_count[key] = percent
-			if percent > @max_percent
-				@max_percent = percent.round
-			end
-		end
+		@habit = current_user.habits.find(params[:id])
+		@weekly_data = chart_it(@habit)
+		@max_percent = @weekly_data.max_by { |_, percentage| percentage }
+		logger.info("Weekly statistics: #{weekly_data}")
 	end
 
 	def destroy
-		user = current_user
-		habits = user.habits.all
-		habit = habits.find(params[:id])
+		habit = current_user.habits.find(params[:id])
 		habit.destroy
 		redirect_to daily_tracker_path
 	end
 
 	private
 	def habit_params
-		params.require(:habit).permit(:title, :description,:weekly_goal)
+		params.require(:habit).permit(:title, :description, :weekly_goal)
 	end
 end
--- habit-hero/app/models/logged_habit.rb
+++ habit-hero/app/models/logged_habit.rb
@@ -1,4 +1,6 @@
 class LoggedHabit < ApplicationRecord
   belongs_to :habit
   belongs_to :user
+  # This can be used in controller as User.logged_habits.current_habits
+  scope :completed, -> { where(date_completed: Time.now.midnight..(Time.now.midnight + 1.day)) }
 end
